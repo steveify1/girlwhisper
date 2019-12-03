7a5033d1c28f290ef30324c955e59d0d8f028031
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-12-03 11:11:19 +0100

    chore(test):setup test environment - setup testing with mocha, chai and mockgoose - setup istanbul reporter

---
+++ src/tests/posts/getPost.spec.js
@@ -0,0 +1,10 @@
+const { assert, expect } = require('chai');
+const request = require('supertest');
+const mockgoose = require('mockgoose');
+
+describe('GET /posts', () => {
+  it('should return a list of posts', done => {
+    expect('a').equals('a');
+    done();
+  });
+});
